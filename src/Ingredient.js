3d3e8264321b7f1d514686b7771cc255b7f33367
kevinfiero <kjf46@cornell.edu>
2020-10-26 16:48:14 -0700

    refactor ingredientList and add data file

--- src/Ingredient.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Ingredient extends Component {
-    render() {
-        return (
-        <div className='flex-row'><input type="checkbox" /><label>{this.props.ingredient}</label></div>
-        )
-    }
-}
